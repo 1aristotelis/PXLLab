bc17b9ecd2bc639a8e29f3516958b2d7a1b6d038
Guillaume De Martino <guillaumedemartino@gmail.com>
2021-10-16 00:00:03 +0200

    give up dark mode

--- src/index.js
+++ src/index.js
@@ -6,38 +6,18 @@ import { ThemeProvider } from "@material-ui/styles";
 
 import App from "./App";
 
-const mode = "dark";
 const theme = createMuiTheme({
   palette: {
-    mode,
-    ...(mode === "bright"
-      ? {
-          primary: {
-            light: "#8b75ff",
-            main: "#5549cd",
-            dark: "#0a209b"
-          },
-          secondary: {
-            light: "#f495ff",
-            main: "#bf64f5",
-            dark: "#8b33c1"
-          }
-        }
-      : {
-          primary: {
-            light: "#8b75ff",
-            main: "#5549cd",
-            dark: "#0a209b"
-          },
-          secondary: {
-            light: "#f495ff",
-            main: "#bf64f5",
-            dark: "#8b33c1"
-          },
-          background: {
-            default: "#0a209b"
-          }
-        })
+    primary: {
+      light: "#8b75ff",
+      main: "#5549cd",
+      dark: "#0a209b"
+    },
+    secondary: {
+      light: "#f495ff",
+      main: "#bf64f5",
+      dark: "#8b33c1"
+    }
   }
 });
 
